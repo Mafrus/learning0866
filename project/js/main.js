fb0a615534b903d7b2a6e37e5578245be7e09f1f
Jorian <jorian@mail.ru>
2022-10-31 14:51:28 +0300

    1031 3

--- project/js/main.js
+++ project/js/main.js
@@ -1,5 +1,7 @@
 /* variables */
 let citylist = ['Москва', 'Немосква', 'Караганда', 'Магадан', 'Люберцы', 'Севастополь', 'Ярославль', 'Вологда', 'Владивосток', 'Барнаул', 'Петрозаводск', 'Самара', 'Саратов', 'Тверь', 'Вашингтон', 'Париж', 'Пермь', 'Екатеринбург', 'Новосибирск', 'Калининград'];
+let rangemin = 0;
+let rangemax = 300;
 
 /* functions */
 function getModalWindow(idname) {
@@ -133,6 +135,50 @@ $(function(){
         // });
     // }
     
+    if ($('#slider-range').length) {
+        $('#slider-range').slider({
+            range: true,
+            min: rangemin,
+            max: rangemax,
+            values: [rangemin, rangemax],
+            slide: function(event, ui) {
+                $('#amount1').val(ui.values[0]);
+                $('#amount2').val(ui.values[1]);
+            }
+        });
+        $('#amount1').on('change', function() {
+            let v1 = +$('#amount1').val();
+            let v2 = +$('#amount2').val();
+            if (v1 > rangemax) {
+                v1 = rangemax;
+            } else if (v1 < rangemin) {
+                v1 = rangemin;
+            }
+            $('#amount1').val(v1);
+            if (v1 > v2) {
+                v2 = v1;
+                $('#amount2').val(v2);
+            }
+            $('#slider-range').slider('values', [v1, v2]);
+        });
+        $('#amount2').on('change', function() {
+            let v1 = +$('#amount1').val();
+            let v2 = +$('#amount2').val();
+            if (v2 > rangemax) {
+                v1 = rangemax;
+            } else if (v2 < rangemin) {
+                v1 = rangemin;
+            }
+            $('#amount2').val(v2);
+            if (v1 > v2) {
+                v1 = v2;
+                $('#amount1').val(v1);
+            }
+            $('#slider-range').slider('values', [v1, v2]);
+        });
+        $('#amount1').val(rangemin);
+        $('#amount2').val(rangemax);
+    }
     
     console.log('just loaded');
 });
